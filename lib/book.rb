5ad77b1bfbf76471e154c41ea56104dfcd7231ff
Judith Awino Omondi <judith.awino.omondi@gmail.com>
2022-08-20 23:13:15 +0300

    Completed

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,22 @@
+class Book
+    attr_accessor :author, :page_count, :genre
+    attr_reader :title
+    def initialize(title)
+        @title = title
+    end
+    # def turn_page= (page)
+    #     @page=page
+    # end
+    # def turn_page
+    #     @page
+    # end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
+caucasian = Book.new("And Then There Were None")
+caucasian.title
 
-
+# book = Book.new
+# book.page = "Flipping the page...wow, you read fast!"
+# puts book.page
